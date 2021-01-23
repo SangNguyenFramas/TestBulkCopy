181c2a6680cb2e940426e6d7b27670a1730ab381
SangNguyenFramas <sang.nguyen@framas.com>
2021-01-23 08:43:01 +0700

    Add project files.

---
+++ TestBulkCopy/DataHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBulkCopy
+{
+    public class DataHelper
+    {
+        private readonly string connectStr = "Data Source=localhost;Initial Catalog=TestDB;Persist Security Info=True;User ID=sa";
+
+
+    }
+}
